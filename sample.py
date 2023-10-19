1148a48e84c8d062608afa29dbba39de0aab8447
psv <o-kami@yandex.ru>
2023-10-19 20:11:33 +0400

    проверяем api

--- sample.py
+++ sample.py
@@ -1,2 +1,12 @@
-print('Начало положено.')
-print('Разбираемся с гитхабом')
+import requests
+
+
+#api_url = 'http://api.open-notify.org/iss-now.json'
+api_url = 'http://numbersapi.com/43/'
+
+response = requests.get(api_url)   # Отправляем GET-запрос и сохраняем ответ в переменной response
+
+if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
+    print(response.text)
+else:
+    print(response.status_code)    # При другом коде ответа выводим этот код
